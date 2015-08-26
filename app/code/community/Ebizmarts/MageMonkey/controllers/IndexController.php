f51f547a81008c37786bf94910502cf1345e8ffa
santisp <santiago@ebizmarts.com>
2015-08-26 15:31:28 -0300

    Added controller to catch cron response.

---
+++ app/code/community/Ebizmarts/MageMonkey/controllers/IndexController.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * MailChimp webhooks controller
+ *
+ * @category   Ebizmarts
+ * @package    Ebizmarts_MageMonkey
+ * @author     Ebizmarts Team <info@ebizmarts.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php
+ */
+class Ebizmarts_MageMonkey_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        $numberDelay = rand(0, 1);
+        if($numberDelay == 0){
+            $delay = 5;
+        }else{
+            $delay = 300;
+        }
+        sleep($delay);
+        $numberResponse = rand(0, 1);
+        if($numberResponse == 0){
+            $responseCode = 500;
+        }else{
+            $responseCode = 200;
+        }
+        $data = array();
+        $data['code'] = $responseCode;
+        $data['delay'] = $delay;
+
+        $this->getResponse()
+            ->setHeader('Content-Type', 'application/json', true)
+            ->setHttpResponseCode($responseCode)
+            ->setBody(json_encode($data));
+    }
+}
